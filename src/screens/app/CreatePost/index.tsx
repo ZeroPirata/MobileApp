eb6b8a3cb4a34b406e079661be3e3de519f25266
ZeroPirata <gabriel0gsbn@gmail.com>
2023-02-11 19:39:27 -0300

    Create Post

--- src/screens/app/CreatePost/index.tsx
+++ src/screens/app/CreatePost/index.tsx
@@ -32,7 +32,12 @@ import { Btn } from "../../../components/button/index";
 import { MaterialIcons } from "@expo/vector-icons";
 import themes from "../../../styles/themes";
 import { db, storage } from "../../../configs/firebase";
-import { ref, uploadBytes } from "firebase/storage";
+import {
+  getDownloadURL,
+  ref,
+  uploadBytes,
+  uploadBytesResumable,
+} from "firebase/storage";
 import { useNavigation } from "@react-navigation/native";
 import { launchImageLibraryAsync, MediaTypeOptions } from "expo-image-picker";
 import { useState } from "react";
@@ -40,14 +45,31 @@ import { useAuthentication } from "../../../hooks/useAuthentication";
 import { uuidv4 } from "@firebase/util";
 import { addDoc, collection } from "firebase/firestore";
 import { ICreatePost } from "../../../interfaces/CreatePost";
+import { RFValue } from "react-native-responsive-fontsize";
 let imageUuidv4: any = [];
+let imageUrl: any = [];
+
+interface ImageState {
+  item: string;
+  index: number;
+  uri: string;
+  id: string;
+  type: string;
+}
 const CreatePost = () => {
   const { user } = useAuthentication();
   const navigation = useNavigation();
 
-  const [image, setImage] = useState([]);
+  const [loadingUpload, setLoadingUpload] = useState(false);
+  const [image, setImage] = useState<ImageState[]>([]);
+  const [value, setValue] = useState({
+    title: "",
+    description: "",
+  });
   const [isLoading, setIsLoagind] = useState(false);
-  const [value, setValue] = useState<ICreatePost>();
+  const [teste2, setTeste2] = useState(false);
+  const [teste, setTeste] = useState(false);
+  const [theArray, setTheArray] = useState<string[]>([]);
   const [uploading, setUploading] = useState(false);
   const pickImage = async () => {
     setIsLoagind(true);
@@ -79,23 +101,27 @@ const CreatePost = () => {
   const handleHomeNavigation = () => {
     navigation.navigate("TabsRoutes");
   };
+
   const uploadImage = async () => {
     if (image.length >= 1) {
-      image.forEach(async (image, index) => {
+      setLoadingUpload(true);
+      image.forEach(async (images, index) => {
         const id = uuidv4();
         imageUuidv4.push({ id: id });
-        const response = await fetch(image.uri);
+        const response = await fetch(images.uri);
         const blob = await response.blob();
         const imageRef = ref(storage, `images/${user?.email}/${id}`);
-        uploadBytes(imageRef, blob);
+        const uploadStatus = uploadBytesResumable(imageRef, blob);
+        uploadStatus.on("state_changed", (snapshot) => {
+          const lastIndex = image.length - 1;
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          if (lastIndex == index && progress == 100) {
+            setTeste(true);
+          }
+        });
         setImage([]);
       });
-      await addDoc(collection(db, "post"), {
-        title: value?.title,
-        description: value?.description,
-        files: imageUuidv4,
-      });
-      navigation.navigate("TabsRoutes");
       return;
     }
     if (image.length == 0) {
@@ -107,7 +133,34 @@ const CreatePost = () => {
       return;
     }
   };
-  return (
+
+  if (teste) {
+    imageUuidv4.forEach(async (files: { id: any }, index: number) => {
+      const lastIndex = imageUuidv4.length - 1;
+      const refUrlImage = ref(storage, `images/${user?.email}/${files.id}`);
+      await getDownloadURL(refUrlImage).then((url) => {
+        setTheArray((oldArray) => [...oldArray, url]);
+      });
+      if (index == lastIndex) {
+        setTeste(false);
+        setTeste2(true);
+      }
+    });
+    imageUuidv4 = [];
+  }
+
+  if (teste2) {
+    navigation.navigate("TabsRoutes");
+    addDoc(collection(db, "post"), {
+      user: user?.email,
+      title: value?.title,
+      description: value?.description,
+      files: theArray,
+    });
+    setTeste2(false);
+    setIsLoagind(false);
+  }
+  return !loadingUpload ? (
     <Container>
       <HeaderBackButton>
         <ButtonArrowBack onPress={handleHomeNavigation}>
@@ -172,6 +225,33 @@ const CreatePost = () => {
         </CreatePostHeader>
       </ScrollView>
     </Container>
+  ) : (
+    <View
+      style={{
+        position: "absolute",
+        backgroundColor: themes.COLORS.TEXT_and_BACKGROUND.GRAY5,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100%",
+        width: "100%",
+      }}
+    >
+      {/*   color: ${({ theme }) => theme.COLORS.HEXTECH_METAL_GOLD.GOLD3};
+    font-family: ${({ theme }) => theme.FONTS.Poppins_600SemiBold_Italic};
+    font-size: ${RFValue(35)}px; */}
+      <Text
+        style={{
+          color: themes.COLORS.HEXTECH_METAL_GOLD.GOLD3,
+          fontFamily: themes.FONTS.Poppins_600SemiBold_Italic,
+          fontSize: RFValue(50),
+        }}
+      >
+        Posting...
+      </Text>
+      <ActivityIndicator size={100} />
+    </View>
   );
 };
 export { CreatePost };
--- src/screens/tab/Home/index.tsx
+++ src/screens/tab/Home/index.tsx
@@ -1,14 +1,144 @@
-import React from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { getDownloadURL, getMetadata, ref } from "firebase/storage";
+import React, { useCallback, useEffect, useState } from "react";
+import { Image, Text, View } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
 import { HeaderProfile } from "../../../components/HeaderProfile";
+import { db, storage } from "../../../configs/firebase";
 
 import { Container } from "./style";
 
 const HomeTab = () => {
+  const [post, setPost] = useState([]);
+  const [image, setImage] = useState("");
+
+  /* const [postUrls, setPostUrls] = useState<{ [key: string]: string[] }>({});
+
+  const receiveMetadataImage = useCallback(
+    async (postId: string, uuidv4: string) => {
+      const forestRef = ref(storage, `images/${uuidv4}`);
+      const url = await getDownloadURL(forestRef);
+      setPostUrls((prevPostUrls) => {
+        const newPostUrls = { ...prevPostUrls };
+        if (!newPostUrls[postId]) {
+          newPostUrls[postId] = [];
+        }
+        newPostUrls[postId].push(url);
+        return newPostUrls;
+      });
+    },
+    []
+  ); */
+  /*   const receiveMetadataImage = useCallback(
+    async (postId: string, uuidv4: string) => {
+      const forestRef = ref(`images/${uuidv4}`);
+      const url = await getDownloadURL(forestRef);
+      setPostUrls((prevPostUrls) => {
+        const newPostUrls = { ...prevPostUrls };
+        if (!newPostUrls[postId]) {
+          newPostUrls[postId] = [];
+        }
+        newPostUrls[postId].push(url);
+        return newPostUrls;
+      });
+    },
+    []
+  ); */
+  const reciveMetadataImage = async (uuidv4: string) => {
+    const forestRef = ref(storage, `images/${uuidv4}`);
+    getDownloadURL(forestRef).then((url) => {
+      return String(url);
+    });
+  };
+  /*   console.log(reciveMetadataImage("a4c31483-d688-4769-84db-c971d0036233"));
+  const reciveUserAttributes = useCallback(async () => {
+    let dados: any = [];
+    const collect = collection(db, "post");
+    const querySnapshot = await getDocs(collect);
+    querySnapshot.forEach((doc) => {
+      dados.push({
+        id: doc.id,
+        body: doc.data(),
+      });
+    });
+    setPost(dados);
+  }, [setPost]);
+
+  useEffect(() => {
+    reciveUserAttributes();
+  }, []); */
+
   return (
     <Container>
       <HeaderProfile />
+      {/* {post.map((primeiroparteAcessarOBody, index) => {
+        return (
+          <View>
+            <Text style={{ fontSize: 10, color: "white" }}>
+              {primeiroparteAcessarOBody.body.title}
+            </Text>
+            <Text style={{ fontSize: 10, color: "white" }}>
+              {primeiroparteAcessarOBody.body.description}
+            </Text>
+            <Text style={{ fontSize: 10, color: "white" }}>
+              {primeiroparteAcessarOBody.body.files &&
+                primeiroparteAcessarOBody.body.files.map((z) => {
+                  return <Text>{z.id}</Text>;
+                })}
+            </Text>
+          </View>
+        );
+      })} */}
     </Container>
   );
 };
 
+/*   console.log(post[0].body.files[0].id); */
+{
+  /*       {post.map((i) => {
+  return (
+    <View>
+      <Text style={{ fontSize: 25, color: "red" }}>{i.body.title}</Text>
+      <Text style={{ fontSize: 10, color: "white" }}>Oi</Text>
+    </View>
+  );
+})} */
+}
+{
+  {
+    /* <FlatList
+    data={post}
+    renderItem={({ item, index }) => (
+      <View key={index}>
+        <Text style={{ fontSize: 25, color: "red" }}>
+          {item.body.title}
+        </Text>
+        <Text>{item.body.description}</Text>
+        {item.body.files && (
+          <FlatList
+            data={item.body.files}
+            renderItem={(files) => (
+              <Text style={{ color: "white" }}>{files}</Text>
+            )}
+          />
+        )}
+      </View>
+    )}
+  /> */
+  }
+  /* <Image
+  source={{
+    uri: "String(receiveMetadataImage(item.id, image.id))",
+  }}
+  style={{ width: 100, height: 100 }}
+/> */
+}
 export { HomeTab };
+/* {item.body.files && (
+              <FlatList
+                data={item.body.files}
+                renderItem={({ item, index }) => (
+                  <Image source={{ uri: item.id }} style={{ width: 100 }} />
+                )}
+              />
+            )} */
